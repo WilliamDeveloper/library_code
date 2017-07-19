c2952705f44b04dd2e5e88841ed56d6478b07a9f
201232 <william_clarck@hotmail.com>
2017-07-19 10:28:30 -0300

    # commit inicial

---
+++ src/br/com/utils/_ExecutaUtils.java
@@ -0,0 +1,30 @@
+package br.com.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class _ExecutaUtils {
+
+    private static String doExecutarComando(String command) {
+
+        StringBuffer output = new StringBuffer();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine())!= null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+}
