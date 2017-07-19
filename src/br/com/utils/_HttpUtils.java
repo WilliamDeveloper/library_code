41e871bab0dfbbbfb5d64aa1f8a3d58bec57c29c
201232 <william_clarck@hotmail.com>
2017-07-19 10:19:57 -0300

    # commit inicial

--- src/br/com/utils/_HttpUtils.java
+++ src/br/com/utils/_HttpUtils.java
@@ -1,4 +1,20 @@
 package br.com.utils;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 public class _HttpUtils {
+
+    public static boolean isIpReacheable(String pIpAddress, int pTimeout){
+        InetAddress inet = null;
+        boolean reachable = false;
+
+        try {
+            inet = InetAddress.getByName(pIpAddress);
+            reachable = inet.isReachable(pTimeout);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return reachable;
+    }
 }
